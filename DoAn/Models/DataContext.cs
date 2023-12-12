6a7cc7931077d7b44298e442cd7f996fc80d2abb
Admin <Admin@DESKTOP-P3L3PMG>
2023-12-12 15:24:08 +0700

    dung 3:23 12/12

--- DoAn/Models/DataContext.cs
+++ DoAn/Models/DataContext.cs
@@ -3,19 +3,25 @@ namespace DoAn.Models
 {
     public class DataContext : DbContext
     {
-      public DataContext(DbContextOptions<DataContext> options) : base(options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
-         
-    public DbSet<Menu> Menus {  get; set; }
-    public DbSet<AboutUs> AboutUs {  get; set; }
-    public DbSet<Service> Services {  get; set; }
-    public DbSet<About> Abouts { get; set; }
-    public DbSet<Services> Service {  get; set; }
-    public DbSet<Event> Events { get; set; }
-    public DbSet<TypeEvent> TypeEvents { get; set; }
-    public DbSet<FoodMenu> FoodMenus { get; set; }
-    public DbSet<TypeFood> TypeFoods { get; set; }
+
+
+
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<AboutUs> AboutUs { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Services> Service { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<TypeEvent> TypeEvents { get; set; }
+        public DbSet<FoodMenu> FoodMenus { get; set; }
+        public DbSet<TypeFood> TypeFoods { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+      
     }
 }
